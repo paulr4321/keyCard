22834b54d4cd70567b0d8570f6a7e308d69264c1
Paul Regan <pregan@ithaca.edu>
2018-03-26 08:57:40 -0400

    fixing Driver doc

--- keycardv1/src/main/java/Driver.java
+++ keycardv1/src/main/java/Driver.java
@@ -1,5 +1,23 @@
 package prim;
 
-public class Driver {
+public static void main(String[] args) {
+    
+	User user = new User();
+	Doors doors = new Doors();
+	boolean check;
+	
+	for (int i= 0; i < length; i++){
+		while(door[i]==user.getDoor()){
+			check = door.checkAccess();
+		}
+	}
+	
+	if(check == true){
+		door.open();
+	}
+	else{
+		door.closed();
+	}
+  }
 
-}
+
