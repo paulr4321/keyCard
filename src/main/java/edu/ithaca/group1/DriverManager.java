85c48cb2124f528771e106f1de5c042e5263db9b
bklopcic <benjaminklopcic@gmail.com>
2018-04-04 09:30:00 -0400

    Made code compile

--- src/main/java/edu/ithaca/group1/DriverManager.java
+++ src/main/java/edu/ithaca/group1/DriverManager.java
@@ -12,7 +12,7 @@ public class DriverManager
 		
 		String s = null;
 
-		User[] userList = {};
+		User[] userList = {new User("00000", "Brenda"), new User("11111", "Kevin")};
 
 		Door door = new Door("", userList);
 
@@ -38,36 +38,36 @@ public class DriverManager
 			System.out.print("Access Denied");
 		}
 		
-		Scanner reader2 = new Scanner(System.in);  // Reading from System.in
-	    System.out.println("Would you like to Add someone from the list? (yes) or (no)");
-	    s = reader2.next();
-	    if(reader2.next()=="yes"){
-	    	Scanner reader3 = new Scanner(System.in);  // Reading from System.in
-		    System.out.println("What is his or her name?");
-		    s = reader3.next();
-		    User newUser = new User("11110", s);
-		    door.addUser(newUser);
-		    
-	    }
-	    
-		Scanner reader4 = new Scanner(System.in);  // Reading from System.in
-	    System.out.println("Would you like to Remove someone from the list? (yes) or (no)");
-	    s = reader2.next();
-	    if(reader2.next()=="yes"){
-	    	try{
-	    		Scanner reader3 = new Scanner(System.in);  // Reading from System.in
-			    System.out.println("What is his or her ID?");
-			    s = reader4.next();
-			    door.removeUser(s);
-			}
-			
-			catch (NumberFormatException a){
-			    System.out.print("Problem");
-			}
-	    	
-		    
-		    
-	    }
+//		Scanner reader2 = new Scanner(System.in);  // Reading from System.in
+//	    System.out.println("Would you like to Add someone from the list? (yes) or (no)");
+//	    s = reader2.next();
+//	    if(s=="yes"){
+//	    	Scanner reader3 = new Scanner(System.in);  // Reading from System.in
+//		    System.out.println("What is his or her name?");
+//		    s = reader3.next();
+//		    User newUser = new User("11110", s);
+//		    door.addUser(newUser);
+//
+//	    }
+//
+//		Scanner reader4 = new Scanner(System.in);  // Reading from System.in
+//	    System.out.println("Would you like to Remove someone from the list? (yes) or (no)");
+//	    s = reader2.next();
+//	    if(reader2.next()=="yes"){
+//	    	try{
+//	    		Scanner reader3 = new Scanner(System.in);  // Reading from System.in
+//			    System.out.println("What is his or her ID?");
+//			    s = reader4.next();
+//			    door.removeUser(s);
+//			}
+//
+//			catch (NumberFormatException a){
+//			    System.out.print("Problem");
+//			}
+//
+//
+//
+//	    }
 	    
 		
 		
