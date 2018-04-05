3203ebbf9f3d13ab1216214ece0712d380ff9ab2
bklopcic <benjaminklopcic@gmail.com>
2018-04-05 14:12:28 -0400

    implemented request class. Fixed driver class test

--- src/test/java/edu/ithaca/group1/RequestTest.java
+++ src/test/java/edu/ithaca/group1/RequestTest.java
@@ -1,5 +1,38 @@
+package edu.ithaca.group1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RequestTest {
+    Request testRequest0;
+    Request testRequest1;
+    Request testRequest2;
+    Request testRequest3;
+
+    @BeforeEach
+    void setUp() {
+        testRequest0 = new Request("11111", "0");
+        testRequest1 = new Request("", "");
+        testRequest2 = new Request("00000", "1");
+        testRequest3 = new Request("43210", "10");
+    }
+
+    @Test
+    void getUserId() {
+        assertEquals("11111", testRequest0.getUserId());
+        assertEquals("", testRequest1.getUserId());
+        assertEquals("00000", testRequest2.getUserId());
+        assertEquals("43210", testRequest3.getUserId());
+    }
+
+    @Test
+    void getDoorId() {
+        assertEquals("0", testRequest0.getDoorId());
+        assertEquals("", testRequest1.getDoorId());
+        assertEquals("1", testRequest2.getDoorId());
+        assertEquals("10", testRequest3.getDoorId());
+    }
 
 }
