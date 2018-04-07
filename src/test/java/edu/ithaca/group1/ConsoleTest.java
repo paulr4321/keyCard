6ccfb5bdd85cce3d16a84c268531221855442b38
Fernando Barreto <Fernando@Fernandos-MacBook-Pro-2.local>
2018-04-06 20:38:51 -0400

    Second iteration of console class

--- src/test/java/edu/ithaca/group1/ConsoleTest.java
+++ src/test/java/edu/ithaca/group1/ConsoleTest.java
@@ -3,6 +3,7 @@ package edu.ithaca.group1;
 import org.junit.jupiter.api.Test;
 import java.io.InputStream;
 import java.io.ByteArrayInputStream;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,14 +13,17 @@ class ConsoleTest {
     void getInputOption() {
 
         String options = "Jump,Run,Sleep,Eat,Fight";
-        Console c = new Console(options);
-
+        Console c = new Console();
         String valid = "3";
         InputStream in = new ByteArrayInputStream(valid.getBytes());
+
         System.setIn(in);
+
         int test = Integer.parseInt(valid);
-        c.listOptions();
-        assertEquals(test, c.getInputOption(), "Incorrect input option");
+
+        c.listOptions(options);
+        assertEquals(test, c.getInputOption(options), "Incorrect input option");
+
 
     }
 }
