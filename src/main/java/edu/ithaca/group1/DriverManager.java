c0359bb78ef6798c6dd5d8282a6d33c9f91ca034
Paul Regan <pregan@ithaca.edu>
2018-04-09 08:32:40 -0400

    adding manager class implementation

--- src/main/java/edu/ithaca/group1/DriverManager.java
+++ src/main/java/edu/ithaca/group1/DriverManager.java
@@ -1,5 +1,5 @@
 package edu.ithaca.group1;
-import org.junit.jupiter.api.Test;
+
 import java.util.Scanner;
 
 
@@ -8,69 +8,43 @@ public class DriverManager
 
 {
 
-	public static void main(String[] args)throws Exception {
+	public DriverManager(){}
+	
+    /**
+     * @addUser takes a door and a user as parameters
+     * @addUser within Manager Class calls addUser within the Door class
+     * the user given will be added to that specific door's permissions
+     */
+	
+	public void addUser(Door door, User user){
+		door.addUser(user);
 		
-		String s = null;
-
-		User[] userList = {new User("00000", "Brenda", ""), new User("11111", "Kevin", "")};
-
-		Door door = new Door("", userList);
-
-		boolean check;
-
-		try{
-		    Scanner reader = new Scanner(System.in);  // Reading from System.in
-		    System.out.println("Enter 5 numbers: ");
-		    s = reader.next();
+		
+	}
+    /**
+     * The Manager is given a list of people approved for the door.
+     * @viewApprovedRequests loops through the list and prints the approved users
+     */
+	public void viewApprovedRequests(User[] userList){
+		
+		for(int i = 0; i< userList.length; i++){
+			System.out.println(userList[i]);
 		}
 		
-		catch (NumberFormatException a){
-		    System.out.print("Problem");
-		}
+	}
+    /**
+     * denyRequest informs the user that their request has been denied 
+     * User not added to the approved list.
+     */
+	public void denyRequest(){
 		
-		check = door.checkUserAccess(s);
+		System.out.println("I am sorry your request has been denied.");
 		
-		
-		if(check == true){
-			System.out.print("Access Granted");
-		}
-		else{
-			System.out.print("Access Denied");
-		}
-		
-//		Scanner reader2 = new Scanner(System.in);  // Reading from System.in
-//	    System.out.println("Would you like to Add someone from the list? (yes) or (no)");
-//	    s = reader2.next();
-//	    if(s=="yes"){
-//	    	Scanner reader3 = new Scanner(System.in);  // Reading from System.in
-//		    System.out.println("What is his or her name?");
-//		    s = reader3.next();
-//		    User newUser = new User("11110", s);
-//		    door.addUser(newUser);
-//
-//	    }
-//
-//		Scanner reader4 = new Scanner(System.in);  // Reading from System.in
-//	    System.out.println("Would you like to Remove someone from the list? (yes) or (no)");
-//	    s = reader2.next();
-//	    if(reader2.next()=="yes"){
-//	    	try{
-//	    		Scanner reader3 = new Scanner(System.in);  // Reading from System.in
-//			    System.out.println("What is his or her ID?");
-//			    s = reader4.next();
-//			    door.removeUser(s);
-//			}
-//
-//			catch (NumberFormatException a){
-//			    System.out.print("Problem");
-//			}
-//
-//
-//
-//	    }
-	    
+	}
+	
+	
+	
 		
 		
 	}
 
-}
