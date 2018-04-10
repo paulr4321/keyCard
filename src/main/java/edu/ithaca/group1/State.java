4209a1859b0a217fe0b16eb6a3575d8045e37938
pkshort <pshort@ithaca.edu>
2018-04-09 21:55:32 -0400

    User Class and Tests

---
+++ src/main/java/edu/ithaca/group1/State.java
@@ -0,0 +1,14 @@
+package edu.ithaca.group1;
+
+public class State {
+
+    private int nextState;
+
+    public State(int myState){
+
+        this.nextState = myState;
+
+    }
+
+    public void run(){}
+}
