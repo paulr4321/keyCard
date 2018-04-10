f36ca5138108a0a9577874375c776338a5d23fd0
Fernando Barreto <Fernando@Fernandos-MacBook-Pro-2.local>
2018-04-10 16:39:01 -0400

    Added comments to Security class

--- src/main/java/edu/ithaca/group1/Security.java
+++ src/main/java/edu/ithaca/group1/Security.java
@@ -5,17 +5,30 @@ public class Security{
     private int securityState = 2;
     private boolean approved = false;
 
+    public Security(){}
+
+    /**
+     * Prints out a list of requests that are awaiting approval from the security rep
+     */
     public void viewPendingRequests()
     {
         System.out.println("List of pending requests...");
     }
 
+    /**
+     * Approves a request from the list of pending requests
+     * @return approved - status of approval should be true since request is approved
+     */
     public boolean ApproveRequest()
     {
         this.approved = true;
         return approved;
     }
 
+    /**
+     * Denies a request from the list of pending requests
+     * @return approved - status of approval should be false since request is denied
+     */
     public boolean DenyRequest()
     {
         this.approved = false;
