6e08559bcd70c9df614a483b990d160641c8c8b3
noclipdelux <vasko1996@yahoo.com>
2018-04-30 22:44:45 -0400

    Implemented record class

--- src/main/java/edu/ithaca/group1/Record.java
+++ src/main/java/edu/ithaca/group1/Record.java
@@ -1,16 +1,18 @@
 package edu.ithaca.group1;
+import java.time.*;
 
 public class Record {
 
     private String userId;
     private String doorId;
-    //private time
+    private LocalDateTime timestamp;
     private Boolean outcome;
 
-    public Record(String userId, String doorId)
-    {
+    // outcome of swipe record is false by default, use setRecordStatus() to change outcome
+    public Record(String userId, String doorId) {
         this.doorId = doorId;
         this.userId = userId;
+        this.outcome = false;
     }
 
     public String getUserId() {
@@ -21,8 +23,52 @@ public class Record {
         return doorId;
     }
 
-    public void getOutcome() {
-        //TODO Handle outcome
+    public Boolean getOutcome() {
+        return outcome;
     }
 
+    private LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setDoorId(String doorId) {
+        this.doorId = doorId;
+    }
+
+    public void setOutcome(Boolean outcome) {
+        this.outcome = outcome;
+    }
+
+    private void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    private void generateTimeStamp() {
+        timestamp = LocalDateTime.now();
+    }
+
+    // Use this method to set the outcome of a swipe, the timestamp is generated when the record status is set
+    public void setRecordStatus(Boolean status){
+        this.outcome = status;
+        generateTimeStamp();
+    }
+
+    // Prints the record in format: "Approved/Denied on hh:mm MM/DD/YYY"
+    public String toString(){
+        String status;
+        int month = this.timestamp.getMonth().getValue();
+        int day = this.timestamp.getDayOfMonth();
+        int year = this.timestamp.getYear();
+        int hour = this.timestamp.getHour();
+        int min = this.timestamp.getMinute();
+
+        if (outcome) status = "Approved";
+        else status = "Denied";
+
+        return status + " on " + hour + ":" + min + " " + month + "/" + day + "/" + year;
+    }
 }
